fe7d9e857d8b5707050d128e420c7a295334ddfc
Basiel Makled <basiel1234@gmail.com>
2023-02-02 16:15:05 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 import './App.css';
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1 style = {{color: "red"}}>Welcome</h1>
+    </div>
+  );
+}
+
+const Word = (props) => {
+  const {word} = useParams()
+  return (
+    <div>
+      <h1 style = {{color: "red"}}>The word you entered is: {word}</h1>
+    </div>
+  );
+}
+
+const Number = (props) => {
+  const {num} = useParams()
+  return (
+    <div>
+      <h1 style = {{color: "red"}}>The number you entered is: {num}</h1>
+    </div>
+  );
+}
+
+const HelloColor = (props) => {
+  const {word,color,bg} = useParams()
+  return (
+    <div>
+      <h1 style = {{color: color}}>{word}</h1>
+    </div>
+  );
+}
+
+
+const HelloColorBg = (props) => {
+  const {word,color,bg} = useParams()
+  return (
+    <div>
+      <h1 style = {{color: color, backgroundColor: bg}}>{word}</h1>
+    </div>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <h1>Routing Practice</h1>
+        <Link to={'/home'}>Home</Link>
+        <br />
+        <Routes>
+          <Route path = "/Home" element = {<Home />}/> 
+          <Route path = "/:word" element = {<Word />}/>
+          <Route path = "/:num" element = {<Number />}/>
+          <Route path = "/:word/:color/" element = {<HelloColor/>}/>
+          <Route path = "/:word/:color/:bg" element = {<HelloColorBg/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
